aeea99b14f6f9450b70e15c7c0818bb4c3a05d9e
dbunta <dmbunta@gmail.com>
2025-04-14 12:10:15 -0400

    first go at repl

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
   "fmt"
   "strings"
+  "bufio"
+  "os"
 )
 
 func main() {
-  fmt.Print("Hello, World!")
+  for {
+    scanner := bufio.NewScanner(os.Stdin)
+    fmt.Print("Pokedex > ")
+    _ = scanner.Scan()
+    text := scanner.Text()
+    words := strings.Split(strings.ToLower(text), " ")
+    fmt.Printf("Your command was: %v\n", words[0])
+  }
 }
 
 func cleanInput(text string) []string {
